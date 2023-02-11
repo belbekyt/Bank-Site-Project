ae88acf43f7a4892495bdb99faa5ff05236033c3
Miłosz Pawłowski <milosz.pawlowski.torun@wp.pl>
2023-02-11 13:21:42 +0100

    Final update
    Final update, offers section done, fully responsive app
    TODO: Hosting and english check

--- src/components/Options/Option/Option.jsx
+++ src/components/Options/Option/Option.jsx
@@ -1,12 +1,10 @@
-import img from '../../../picture/loan.png';
-
-const Option = () => {
+const Option = ({ photo, heading, text }) => {
     return (
-        <div className="w-3/4 mb-14 rounded overflow-hidden cursor-pointer bg-slate-100 hover:bg-slate-200">
-            <img src={img} alt="loan"/>
+        <div className='offers-card'>
+            <img src={photo} alt="loan"/>
             <div className='p-5'>
-                <h3 className='text-xl mb-2 font-semibold'>Create account here</h3>
-                <p>You can easily register a new account by our internet site</p>
+                <h3 className='offers-card-text'>{heading}</h3>
+                <p>{text}</p>
             </div>
         </div>
     )
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,17 @@
-import Option from "./Option/Option";
+import Option from './Option/Option';
+import photo1 from '../../picture/app.png';
+import photo2 from '../../picture/family.png';
+import photo3 from '../../picture/loan.png';
 
 const Options = () => {
     return (
-        <div className="w-full flex flex-col items-center">
-            <h2 className="text-2xl my-10">Do it easily without going out</h2>
-            <Option />
+        <div className='w-full flex flex-col items-center bg-slate-200'>
+            <h2 className='text-2xl my-10'>Do it easily without going out</h2>
+            <div className='w-full flex flex-col items-center lg:flex-row lg:justify-around'>
+                <Option photo={photo1} heading='Use on mobile' text='Now you can do everything with your bank account and finances by a mobile phone in our mobile app.'/>
+                <Option photo={photo2} heading='Family account' text='One of our features is that you can open an family account and start saving money for your children.'/>
+                <Option photo={photo3} heading='Loans everywhere' text='Need fast a small amount of money? You can easily get a loan to 7.000 PLN within 5 minutes in home.'/>
+            </div>
         </div>
     )
 }
